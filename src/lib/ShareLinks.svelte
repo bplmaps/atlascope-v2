6536e944f82faddcb8478efd29364ef0fde2b62c
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2023-01-12 14:09:01 -0500

    share controls fix

---
+++ src/lib/ShareLinks.svelte
@@ -0,0 +1,66 @@
+<script>
+  import Fa from "svelte-fa";
+  import {faCopy, faMobileAlt, faStreetView} from "@fortawesome/free-solid-svg-icons";
+
+  import LightIconButton from "./LightIconButton.svelte";
+
+  let showView = false;
+
+  export let baseURL;
+  export let mapState;
+
+  let urlField;
+
+  const appURL = baseURL;
+  $: viewURL = `${baseURL}/#/view:share$mode:${mapState.viewMode}$center:${mapState.center}$zoom:${mapState.zoom}$base:${mapState
+                .layers.base.id}$overlay:${mapState.layers.overlay.id}`;
+
+
+  function copyURL() {
+    urlField.select();
+    urlField.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(urlField.value);
+  }
+  
+</script>
+
+<div>
+  
+  <div class="md:flex md:flex-row w-full">
+    <div class="pt-2 pb-1 text-center">
+      
+      <div class="inline-flex items-center">
+        <span class="mr-2 text-sm font-medium {!showView ? 'text-red-900' : 'text-gray-500'} text-center ">App link</span>
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" bind:checked={showView} class="sr-only peer">
+          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+        </label>
+        <span class="ml-2 text-sm font-medium {showView ? 'text-red-900' : 'text-gray-500'} text-center">View link</span>
+      </div>
+
+    </div>
+    <div class="px-3 flex-grow flex">
+      <input
+              type="text"
+              id="share-app-url"
+              class="flex-grow shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value="{showView ? appURL : viewURL}"
+              bind:this="{urlField}"
+            />
+            <button class="ml-2" on:click={copyURL}><Fa icon={faCopy} class="inline" /></button>
+
+    </div>
+  </div>
+
+  {#if navigator.share}
+        <div class="flex flex-wrap mt-2">
+          <LightIconButton label="Share app" icon="{faMobileAlt}" on:click={()=>{navigator.share({title: "Atlascope", url: appURL})}} />
+          <LightIconButton label="Share view" icon="{faStreetView}" on:click={()=>{navigator.share({title: "Atlascope", url: viewURL})}} />
+  </div>
+  {/if}
+
+</div>
+
+<style>
+  
+</style>
