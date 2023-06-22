87dffaed97245f33d3e730d2c5541c63a4ac8565
Emily Bowe <emilyrbowe@users.noreply.github.com>
2023-06-22 15:58:45 -0400

    google tag manager update

--- src/App.svelte
+++ src/App.svelte
@@ -93,11 +93,24 @@
         );
       });
   });
+
+  // Google Tag Manager script
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer',`${instanceVariables.gaTMContainerId}`);
+
 </script>
 
 <svelte:head>
   <title>Atlascope {instanceVariables.name} · {instanceVariables.institutionalShortName}</title>
 </svelte:head>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id`${instanceVariables.gaTMContainerId}`"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
 <div id="wraps-all">
   {#if $appState.layersLoaded}
