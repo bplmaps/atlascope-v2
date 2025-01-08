f92beab277021a863220472b61d9c6a333b882c7
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2025-01-08 16:42:45 -0500

    filter by extents in searchModal

--- src/lib/SearchModal.svelte
+++ src/lib/SearchModal.svelte
@@ -2,6 +2,7 @@
   import Fa from "svelte-fa";
   import {
     faCircleArrowRight,
+    faCropSimple,
     faSearchLocation,
   } from "@fortawesome/free-solid-svg-icons";
   
@@ -11,11 +12,29 @@
 
   import instanceVariables from "../config/instance.json";
 
+  // ITS: import insideChecker
+
+  import { insideChecker } from "./helpers/intersector";
+  
+  import { Log } from "faunadb";
+
   let dispatch = createEventDispatcher();
-  let searchResults;
-  let searchText = "";
+
+  // ITS: instantiate some new variables, including
+  // `arr` for holding filtered results;
+  // `filtered` for holding the `arr`;
+  // `inside` for holding the status of the
+  // `insideChecker` results; and
+  // `volumePoly` for holding the dissolved extents geometry
 
   let debounceTimer;
+  let searchResults;
+  let arr;
+  let filtered;
+  let inside;
+  let volumePoly;
+  let searchText = "";
+
   function debounceSearch(e) {
     clearTimeout(debounceTimer);
     debounceTimer = setTimeout(() => {
@@ -24,16 +43,46 @@
   }
 
   function executeSearch(searchString) {
-    fetch(
-      `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?text=${searchString}&f=json&searchExtent=${instanceVariables.maxExtent.join(
-        ","
-      )}`
-    )
+    fetch(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?text=${searchString}&f=json&searchExtent=${instanceVariables.maxExtent.join(",")}`)
       .then((d) => d.json())
       .then((d) => {
+        console.log(searchResults)
+        console.log('execute search: define search results globally')
         searchResults = d;
+        filter(searchResults)
       });
   }
+
+  // ITS: filter function which will take search results as its argument
+
+  function filter(sr) {
+    filtered=[]
+    console.log("filter")
+    for (const s in sr.suggestions) {
+      fetch(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?&magicKey="${sr.suggestions[s].magicKey}"&f=json`)
+      .then((d) => d.json())
+      .then((d) => {
+        let point = d.candidates[0].location
+        inside = insideChecker(point, volumePoly)
+        if (inside === false) {
+          sr.suggestions.pop()
+        } else {
+        }
+        filtered=sr.suggestions
+      })
+    }
+  }
+
+  // ITS: fetch polygon data
+
+  function getVolumesPoly() {
+    return fetch(instanceVariables.footprintsDissolved)
+      .then((d) => d.json())
+      .then((d) => {
+        const r = d.features[0].geometry
+        volumePoly=r
+      })
+    }
 
   function handleSelection(d) {
     searchResults = null;
@@ -54,6 +103,10 @@
 </script>
 
 <section id="search-modal">
+
+  <!-- ITS: func to get poly data; just hang on to it globally for now -->
+
+  {getVolumesPoly()}
   <div class="modal-outer">
     <div class="modal-inner relative w-full">
       <h1 class="text-xl font-bold">
@@ -77,7 +130,11 @@
       {#if searchResults && searchResults.suggestions && searchResults.suggestions.length > 0}
         <div>
           <ul>
-            {#each searchResults.suggestions as result}
+
+            <!-- ITS: loop `filtered` list -->
+
+            {console.log(searchResults)}
+            {#each filtered as result}
               <li
                 class="text-gray-700 ml-2 mb-1 cursor-pointer text-md hover:text-red-900 group"
                 on:click={() => {
--- src/lib/helpers/intersector.js
+++ src/lib/helpers/intersector.js
@@ -1,9 +1,9 @@
 import intersect from "@turf/intersect";
 import bboxPolygon from "@turf/bbox-polygon";
 import area from "@turf/area";
-import { featureCollection, multiPolygon } from "@turf/turf";
+import { featureCollection, multiPolygon, point} from "@turf/turf";
 import {transformExtent} from 'ol/proj';
-
+import { pointsWithinPolygon } from "@turf/points-within-polygon";
 
 export const intersector = (footprint, extent) => {
     let extent4326 = transformExtent(extent,'EPSG:3857','EPSG:4326')
@@ -13,5 +13,16 @@ export const intersector = (footprint, extent) => {
         return 0;
     } else {
         return area(intersection)/area(extentPolygon);
+    }
+}
+
+export const insideChecker = (ll,poly) => {
+    let result = point(Object.values(ll))
+    let searchWithin = multiPolygon(poly.coordinates)
+    let ptsWithin = pointsWithinPolygon(result, searchWithin);
+    if (ptsWithin.features.length > 0) {
+        return true;
+    } else {
+        return false
     }
 }
