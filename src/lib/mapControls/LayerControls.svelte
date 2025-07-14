11baade6f40901ac6525c1a1e1b5f93f78761e93
itspangler <itspangler@gmail.com>
2025-07-14 09:37:43 -0400

    Update LayerControls.svelte
    conditional logic for glass mode buttons

--- src/lib/mapControls/LayerControls.svelte
+++ src/lib/mapControls/LayerControls.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { appState } from "../state.svelte.js";
+    import { mapState, appState } from "../state.svelte.js";
 
     import LayerChooserDropupMenu from "./LayerChooserDropupMenu.svelte";
     import LightIconButton from "../ui/LightIconButton.svelte";
@@ -10,6 +10,18 @@
 </script>
 <h2 class="md:hidden text-xl font-bold mb-2">Atlases</h2>
       <div class="flex max-w-full flex-wrap">
+        {#if mapState.viewMode === "glass"}
+        <div class="mr-4">
+            <LayerChooserDropupMenu
+            layerName="base"
+          />
+        </div>
+        <div class="mr-4">
+          <LayerChooserDropupMenu
+            layerName="overlay"
+          />
+        </div>
+        {:else}
         <div class="mr-4">
           <LayerChooserDropupMenu
             layerName="overlay"
@@ -20,6 +32,7 @@
             layerName="base"
           />
         </div>
+        {/if}
         <div>
           <LightIconButton
             label="Bibliographic information"
