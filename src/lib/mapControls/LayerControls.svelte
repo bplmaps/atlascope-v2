c51683c84a226e1c8aa665d710f216bc1e496149
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2025-07-14 10:16:38 -0400

    tidy layer order swapper

--- src/lib/mapControls/LayerControls.svelte
+++ src/lib/mapControls/LayerControls.svelte
@@ -1,45 +1,32 @@
 <script>
-    import { mapState, appState } from "../state.svelte.js";
+  import { mapState, appState } from "../state.svelte.js";
 
-    import LayerChooserDropupMenu from "./LayerChooserDropupMenu.svelte";
-    import LightIconButton from "../ui/LightIconButton.svelte";
+  import LayerChooserDropupMenu from "./LayerChooserDropupMenu.svelte";
+  import LightIconButton from "../ui/LightIconButton.svelte";
 
-    import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
-
-
+  import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
 </script>
+
 <h2 class="md:hidden text-xl font-bold mb-2">Atlases</h2>
-      <div class="flex max-w-full flex-wrap">
-        {#if mapState.viewMode === "glass"}
-        <div class="mr-4">
-            <LayerChooserDropupMenu
-            layerName="base"
-          />
-        </div>
-        <div class="mr-4">
-          <LayerChooserDropupMenu
-            layerName="overlay"
-          />
-        </div>
-        {:else}
-        <div class="mr-4">
-          <LayerChooserDropupMenu
-            layerName="overlay"
-          />
-        </div>
-        <div class="mr-4">
-            <LayerChooserDropupMenu
-            layerName="base"
-          />
-        </div>
-        {/if}
-        <div>
-          <LightIconButton
-            label="Bibliographic information"
-            icon={faBookBookmark}
-            on:click={() => {
-              appState.modals.biblio = true;
-            }}
-          />
-        </div>
-      </div>
+<div class="flex max-w-full flex-wrap">
+  <div class="mr-4">
+    <LayerChooserDropupMenu
+      layerName={mapState.viewMode === "glass" ? "overlay" : "base"}
+    />
+  </div>
+  <div class="mr-4">
+    <LayerChooserDropupMenu
+      layerName={mapState.viewMode === "glass" ? "base" : "overlay"}
+    />
+  </div>
+
+  <div>
+    <LightIconButton
+      label="Bibliographic information"
+      icon={faBookBookmark}
+      on:click={() => {
+        appState.modals.biblio = true;
+      }}
+    />
+  </div>
+</div>
