13eb92fc510a7bbe62e8d1d1c17468436abf7ac4
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2025-10-23 16:32:57 -0400

    add massmapper to research-connections.js

--- src/config/research-connections.js
+++ src/config/research-connections.js
@@ -1,10 +1,17 @@
 export const bboxFunctions = [
-    {"name": "Search more maps here",
-    "searchFunction": function(bbox) { return `https://collections.leventhalmap.org/search?q=&utf8=✓&view=split&bbox=${bbox.join('%20')}` }
+    {
+        "name": "Search more maps here",
+        "searchFunction": function (bbox) { return `https://collections.leventhalmap.org/search?q=&utf8=✓&view=split&bbox=${bbox.join('%20')}` }
     }
     ,
 
-    {"name": "Search photographs here",
-    "searchFunction": function(bbox) { return `https://www.digitalcommonwealth.org/search?coordinates=%5B${bbox[1]}%2C${bbox[0]}%20TO%20${bbox[3]}%2C${bbox[2]}%5D&spatial_search_type=bbox&view=gallery&f%5Bgenre_basic_ssim%5D%5B%5D=Photographs` }
+    {
+        "name": "Search photographs here",
+        "searchFunction": function (bbox) { return `https://www.digitalcommonwealth.org/search?coordinates=%5B${bbox[1]}%2C${bbox[0]}%20TO%20${bbox[3]}%2C${bbox[2]}%5D&spatial_search_type=bbox&view=gallery&f%5Bgenre_basic_ssim%5D%5B%5D=Photographs` }
+    },
+
+    {
+        "name": "Search parcels in MassMapper",
+        "searchFunction": function (bbox) { return `https://maps.massgis.digital.mass.gov/MassMapper/MassMapper.html?bl=MassGIS%20Basemap__100&l=Basemaps_L3Parcels____ON__100&b=${bbox.join(',')}` }
     }
 ]
