34f2848cc2e22dacc5ce063a1a458f5f2cf60d0c
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2025-12-12 16:57:50 -0500

    Update TourListModal.svelte

--- src/lib/tours/TourListModal.svelte
+++ src/lib/tours/TourListModal.svelte
@@ -2,7 +2,6 @@
   import Fa from "svelte-fa";
 
   import {
-    faArrowCircleRight,
     faCircleArrowRight,
     faHiking,
   } from "@fortawesome/free-solid-svg-icons";
@@ -31,55 +30,59 @@
 
 <section id="search-modal">
   <div class="modal-outer">
-    <div class="modal-inner relative w-full">
-      <h1 class="text-xl font-bold mb-3">
+    <div class="modal-inner relative w-full md:w-[60%] mx-auto">
+      <h1
+        class="sticky absolute top-0 left-0 right-0 bg-white/90 text-xl font-bold p-4"
+      >
         <Fa icon={faHiking} class="inline mr-2" />Take a tour
       </h1>
-
-      {#if loadingFlag}
-        <div class="text-center text-gray-600 w-full p-5">
-          <div>
-            <svg
-              aria-hidden="true"
-              class="inline mr-2 w-8 h-8 text-gray-200 animate-spin fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
+      <div class="">
+        {#if loadingFlag}
+          <div class="text-center text-gray-600 w-full p-5">
+            <div>
+              <svg
+                aria-hidden="true"
+                class="inline mr-2 w-8 h-8 text-gray-200 animate-spin fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            </div>
+            Loading tours ...
           </div>
-          Loading tours ...
-        </div>
-      {:else}
-        <div>
-          <ul>
-            {#each tours as tour}
-              {#if tour?.metadataJson?.title}
-                <li
-                  onclick={() => {
-                    startTour(tour.id);
-                  }}
-                  class="text-gray-700 py-2 border-b-2 border-gray-200 cursor-pointer text-md hover:text-red-900 group"
-                >
-                  <Fa
-                    icon={faCircleArrowRight}
-                    class="mr-1 inline text-sm text-slate-100 group-hover:text-red-900"
-                  />
-                  {tour.metadataJson.title}
-                </li>
-              {/if}
-            {/each}
-          </ul>
-        </div>
-      {/if}
+        {:else}
+          <div class="px-5">
+            <ul>
+              {#each tours as tour}
+                {#if tour?.metadataJson?.title}
+                  <li
+                    onclick={() => {
+                      startTour(tour.id);
+                    }}
+                    class="text-gray-700 py-2 border-b-2 border-gray-200 cursor-pointer text-md hover:text-red-900 group"
+                  >
+                    <Fa
+                      icon={faCircleArrowRight}
+                      class="mr-1 inline text-sm text-slate-100 group-hover:text-red-900"
+                    />
+                    {tour.metadataJson.title}
+                  </li>
+                {/if}
+              {/each}
+            </ul>
+          </div>
+        {/if}
+         <div class="text-gray-700 bg-gray-100 px-8 py-8 italic text-sm"><a class="underline text-red-900" href="mailto:frontdesk@leventhalmap.org">Get in touch</a> to learn how you can write your own Atlascope tour</div>
+      </div>
     </div>
   </div>
 </section>
@@ -100,7 +103,8 @@
   .modal-inner {
     background-color: white;
     border-radius: 5px;
-    padding: 20px;
     min-height: 200px;
+    max-height: 500px;
+    overflow-y: auto;
   }
 </style>
