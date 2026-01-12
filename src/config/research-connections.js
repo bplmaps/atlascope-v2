9f24518fa4fa08dc71b85f84298bbeea3c09ceeb
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2026-01-12 09:31:30 -0500

    Update research-connections.js
    remove search by maps button

--- src/config/research-connections.js
+++ src/config/research-connections.js
@@ -1,12 +1,5 @@
 export const bboxFunctions = [
     {
-        "name": "Search more maps here",
-        "searchFunction": function (bbox) { return `https://collections.leventhalmap.org/search?q=&utf8=✓&view=split&bbox=${bbox.join('%20')}` },
-        "hiddenOnMobile": false
-    }
-    ,
-
-    {
         "name": "Search photographs here",
         "searchFunction": function (bbox) { return `https://www.digitalcommonwealth.org/search?coordinates=%5B${bbox[1]}%2C${bbox[0]}%20TO%20${bbox[3]}%2C${bbox[2]}%5D&spatial_search_type=bbox&view=gallery&f%5Bgenre_basic_ssim%5D%5B%5D=Photographs` },
         "hiddenOnMobile": false
